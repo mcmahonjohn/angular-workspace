82619b554e998115d758c8a785185d770c383124
mcmahonjohn <18350881+mcmahonjohn@users.noreply.github.com>
2025-11-27 04:51:59 +0000

    feat: add TypeScript definition tests for SignalInputDemoComponent

---
+++ projects/my-app/src/app/components/signal-input-demo/signal-input-demo.component.tsd.ts
@@ -0,0 +1,16 @@
+import { expectType } from 'tsd';
+
+import { SignalInputDemoComponent } from './signal-input-demo.component';
+
+const component = new SignalInputDemoComponent();
+
+// inputSignal should be a signal that returns string|null
+expectType<string | null>(component.inputSignal());
+
+// Should accept string
+component.inputSignal('foo');
+expectType<string | null>(component.inputSignal());
+
+// Should accept null
+component.inputSignal(null);
+expectType<string | null>(component.inputSignal());

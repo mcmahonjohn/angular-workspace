1ddd63df5d4d60abb5b473cd6ff2fc33ff9a8971
mcmahonjohn <18350881+mcmahonjohn@users.noreply.github.com>
2025-12-02 05:07:41 +0000

    chore: Move other my-lib elements to their own folder

--- projects/my-lib/src/public-api.ts
+++ projects/my-lib/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of my-lib
  */
 
-export * from './lib/my-lib.service';
-export * from './lib/my-lib.component';
+export * from './lib/services/my-lib/my-lib.service';
+export * from './lib/components/my-lib/my-lib.component';
 export * from './lib/components/horizontal-nav-bar/horizontal-nav-bar.component';
 export * from './lib/components/vertical-nav-bar/vertical-nav-bar.component';

9f1fa030645b81705b2af0562fc4ef2bc282f351
mcmahonjohn <18350881+mcmahonjohn@users.noreply.github.com>
2025-12-23 22:21:54 +0000

    chore: refactor build script to improve schema file and template copying

--- eslint-plugin-angular-signal/schematics/build.sh
+++ eslint-plugin-angular-signal/schematics/build.sh
@@ -35,9 +35,35 @@ fi
 echo "Copying collection.json..."
 cp "$SCHEMATICS_DIR/collection.json" "$DIST_DIR/collection.json"
 
-# Create necessary directories (add more as needed)
-echo "Creating directories..."
-mkdir -p "$DIST_DIR/ng-add"
+# Copy schema files and templates
+echo "Copying schema files and templates..."
+
+for version in ng-add; do
+
+    if [ "$version" == "ng-add" ]; then
+        SUB_DIR=$version
+    else
+        SUB_DIR="update-$version"
+    fi
+
+    mkdir -p "$DIST_DIR/$SUB_DIR"
+
+    cp "$SCHEMATICS_DIR/$SUB_DIR/schema.json" "$DIST_DIR/$SUB_DIR/schema.json"
+
+    # Copy static directory if it exists
+    if [ -d "$SCHEMATICS_DIR/$SUB_DIR/static" ]; then
+        mkdir -p "$DIST_DIR/$SUB_DIR/static"
+        cp -r "$SCHEMATICS_DIR/$SUB_DIR/static/"* "$DIST_DIR/$SUB_DIR/static/"
+    fi
+
+    # Copy template directory if it exists
+
+    if [ -d "$SCHEMATICS_DIR/$SUB_DIR/templates" ] && [ "$(ls -A $SCHEMATICS_DIR/$SUB_DIR/templates)" ]; then
+        mkdir -p "$DIST_DIR/$SUB_DIR/templates"
+        cp -r "$SCHEMATICS_DIR/$SUB_DIR/templates/"* "$DIST_DIR/$SUB_DIR/templates/"
+    fi
+done
+
 
 # Remove spec files for production build
 if [[ "$1" == "prod" || "$1" == "--prod" || "$1" == "-p" ]]; then
